4783068f6043acf54d6a620aaf118eed51d693c6
AlineDominique <alinedomi.silva@gmail.com>
2020-04-08 21:02:46 -0300

    Criado os atributos de instâncias nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,25 @@
 class Pessoa:
+    '''
+    O atributos de instância de objetos são criados atraves do método especial __init__.
+    '''
+    def __init__(self,nome=None, idade=None):
+        '''
+        Explicação  no video:
+        :param nome: refere-se a uma varável do contexto do código.
+        :param self.nome: é nome do objeto self
+        '''
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Dominique')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Aline'
+    print(p.nome)
+    print(p.idade)
