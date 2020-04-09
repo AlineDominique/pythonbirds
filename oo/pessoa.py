f07fb91d3b80a49488e1f4f3894a564945e91c94
AlineDominique <alinedomi.silva@gmail.com>
2020-04-08 21:18:01 -0300

    Criado os atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,24 +2,27 @@ class Pessoa:
     '''
     O atributos de instância de objetos são criados atraves do método especial __init__.
     '''
-    def __init__(self,nome=None, idade=None):
+    def __init__(self,*filhos,nome=None, idade=None):
         '''
         Explicação  no video:
         :param nome: refere-se a uma varável do contexto do código.
         :param self.nome: é nome do objeto self
+        :param filhos: um atributo complexo,
         '''
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Dominique')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Aline'
-    print(p.nome)
-    print(p.idade)
+    aline = Pessoa(nome='Aline')
+    maria = Pessoa(aline,nome='Maria Benedita')
+    print(Pessoa.cumprimentar(maria))
+    print(id(maria))
+    print(maria.cumprimentar())
+    print(maria.nome)
+    print(maria.idade)
+    for filho in maria.filhos:
+        print(filho.nome)
