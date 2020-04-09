3f01f15bf3eedef2cf6d657f7102b4aef8a68f5d
AlineDominique <alinedomi.silva@gmail.com>
2020-04-08 21:46:08 -0300

    Criado e removido atributo dinâmico do objeto tipo Pessoa.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,15 @@
 class Pessoa:
     '''
     O atributos de instância de objetos são criados atraves do método especial __init__.
+     Em python é possível criar atributos em tempo de execução para um objeto, mas este atributo é apenas para o objeto
+     em que foi relacionado, nem um outro obejto do mesmo tipo terá este atributo. Como exemplo na linha 30.
     '''
     def __init__(self,*filhos,nome=None, idade=None):
         '''
         Explicação  no video:
         :param nome: refere-se a uma varável do contexto do código.
         :param self.nome: é nome do objeto self
-        :param filhos: um atributo complexo,
+        :param filhos: um atributo complexo
         '''
         self.idade = idade
         self.nome = nome
@@ -26,3 +28,15 @@ if __name__ == '__main__':
     print(maria.idade)
     for filho in maria.filhos:
         print(filho.nome)
+    maria.sobrenome = 'Santos'
+    print(maria.sobrenome)
+    '''
+    O atributo especial __dict__ mostra os atributos de instância do objeto, os criados no __init__ e os em tempo de execução.
+    É possível remover atributos dinamicamente também, usando o método <del + objeto.atributo>. Exemplo linha:39.
+    Apesar da linguagem oferecer manipulação dinamica dos atributos, não é uma boa pratica. E dever ser usado em ocasiões
+    específicas.     
+    '''
+    print(maria.__dict__)
+    print(aline.__dict__)
+    del maria.idade
+    print(maria.__dict__)
